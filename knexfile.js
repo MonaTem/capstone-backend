68115d91adc52af14c8308c17921310ba7ff780e
Mona Temchin <monatemchin@gmail.com>
2018-03-29 13:58:28 -0400

    Added migrations
    Added knexfile, db.js, and Knex migrations

---
+++ knexfile.js
@@ -0,0 +1,22 @@
+// module.exports = {
+// development: {
+// client: ‘pg’,
+// connection: ‘postgres://localhost:5432/capstone_stories’
+// }
+// }
+/*if (process.env.APP_MODE === undefined) {
+ throw Error('APP_MODE not set in application environment');
+}
+*/
+var dotenv = require('dotenv').config();
+
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
+  }
+};
