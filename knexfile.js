ae907fe81c5e76c7a31cc7419c98adbac85c5ff0
Mona Temchin <monatemchin@gmail.com>
2018-04-08 18:50:33 -0400

    Removed package-lock.json to fix failed Heroku deployment, modified knexfile, package.json

--- knexfile.js
+++ knexfile.js
@@ -1,33 +1,33 @@
 
-module.exports = {
-
-  development: {
-    client: 'pg',
-    connection: 'postgres://localhost:5432/capstone_stories'
-  }
-
-};
-
-
-
-
-
-// var dotenv = require('dotenv').config();
-
-
-/*if (process.env.APP_MODE === undefined) {
- throw Error('APP_MODE not set in application environment');
-}
-*/
-
-//
 // module.exports = {
+//
 //   development: {
 //     client: 'pg',
-//     connection: process.env.DATABASE_URL
-//   },
-//   production: {
-//     client: 'pg',
-//     connection: process.env.DATABASE_URL
+//     connection: 'postgres://localhost:5432/capstone_stories'
 //   }
+//
 // };
+
+
+
+
+
+var dotenv = require('dotenv').config();
+
+
+if (process.env.APP_MODE === undefined) {
+ throw Error('APP_MODE not set in application environment');
+}
+
+
+
+ module.exports = {
+  development: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
+   },
+   production: {
+     client: 'pg',
+     connection: process.env.DATABASE_URL
+   }
+ };
